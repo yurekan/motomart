9d8d30102ed3c388c4f26c0def224839d6eb1c4c
yurekan <yurekanramasamy@gmail.com>
2024-10-02 23:09:36 +0530

    created motomart accessories page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgbCarouselModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
