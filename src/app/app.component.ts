03e65e7ba5ced1d54eb5b94560e572086c41e66f
yurekan <yurekanramasamy@gmail.com>
2024-10-03 19:18:32 +0530

    added bike components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,52 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 
+interface Card {
+  img: string;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgbCarouselModule],
+  imports: [RouterOutlet, NgbCarouselModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'motomart';
+
+  cards: Card[] = [
+    { img: '../assets/Honda.webp', name: 'Honda' },
+    { img: '../assets/TVS.webp', name: 'TVS' },
+    { img: '../assets/royal_enfield.webp', name: 'Royal Enfield' },
+    { img: '../assets/KTM.webp', name: 'KTM' },
+    { img: '../assets/Yamaha.webp', name: 'Yamaha' },
+    { img: '../assets/Bajaj.webp', name: 'Bajaj' },
+    { img: '../assets/Suzuki.webp', name: 'Suzuki' },
+    { img: '../assets/Kawasaki.webp', name: 'Kawasaki' }
+  ];
+
+  slides: Card[][] = [];
+
+  currentIndex = 0;
+  itemsPerPage = 4;
+
+  get displayedCards() {
+    return this.cards.slice(this.currentIndex, this.currentIndex + this.itemsPerPage);
+  }
+
+  next() {
+    if (this.currentIndex < this.cards.length - this.itemsPerPage) {
+      this.currentIndex++;
+    }
+  }
+
+  previous() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    }
+  }
 }
